5611770cd3e386bd0b72a567543b72415a002b28
DimovDimo <d.dimov1996@gmail.com>
2020-07-21 15:57:54 +0300

    added function getItems

--- app/resources/js/matrix-rain.js
+++ app/resources/js/matrix-rain.js
@@ -1,8 +1,14 @@
 let canvasMatrixRain = document.getElementById("matrix-rain");
 
 canvasFullScreen(canvasMatrixRain);
+let items = getItems();
 
 function canvasFullScreen(canvasMatrixRain) {
     canvasMatrixRain.height = window.innerHeight;
     canvasMatrixRain.width = window.innerWidth;
+}
+
+function getItems() {
+    let input = "01";	
+	return input.split("");
 }
