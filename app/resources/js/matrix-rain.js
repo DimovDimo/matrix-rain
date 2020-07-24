d3ed0dc69cb4168e794999bb7c916ccbf4dec399
DimovDimo <d.dimov1996@gmail.com>
2020-07-24 08:18:43 +0300

    added canvasMatrixRainContext

--- app/resources/js/matrix-rain.js
+++ app/resources/js/matrix-rain.js
@@ -1,11 +1,18 @@
 const fontSizeConstant = 10;
 const particlesConstant = 1;
+const canvasIdConstant = "matrix-rain";
+const canvasContextConstant = "2d";
 
-let canvasMatrixRain = document.getElementById("matrix-rain");
+let canvasMatrixRain = document.getElementById(canvasIdConstant);
+let canvasMatrixRainContext = canvasMatrixRain.getContext(canvasContextConstant);
 
 canvasFullScreen();
 let items = getItems();
 let particles = getParticles();
+
+function matrixRain() {
+
+}
 
 function canvasFullScreen() {
     canvasMatrixRain.height = window.innerHeight;
