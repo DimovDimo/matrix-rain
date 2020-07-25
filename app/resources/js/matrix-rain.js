6933f571e175620236c69a44539544420f100361
DimovDimo <d.dimov1996@gmail.com>
2020-07-25 11:21:16 +0300

    added function designCssMatrixRain

--- app/resources/js/matrix-rain.js
+++ app/resources/js/matrix-rain.js
@@ -2,6 +2,8 @@ const fontSizeConstant = 10;
 const particlesConstant = 1;
 const canvasIdConstant = "matrix-rain";
 const canvasContextConstant = "2d";
+const canvasBackgroundColorConstant = "rgba(0, 0, 0, 0.1)";
+const canvasTextColorConstant = "#0F0";
 
 let canvasMatrixRain = document.getElementById(canvasIdConstant);
 let canvasMatrixRainContext = canvasMatrixRain.getContext(canvasContextConstant);
@@ -10,13 +12,14 @@ canvasFullScreen();
 let items = getItems();
 let particles = getParticles();
 
-function matrixRain() {
-
+function matrixRain()
+{
+	designCssMatrixRain();
 }
 
 function canvasFullScreen() {
-    canvasMatrixRain.height = window.innerHeight;
     canvasMatrixRain.width = window.innerWidth;
+	canvasMatrixRain.height = window.innerHeight;
 }
 
 function getItems() {
@@ -36,4 +39,10 @@ function getParticles() {
 	}
 	
 	return particles;
+}
+
+function designCssMatrixRain() {
+	canvasMatrixRainContext.fillStyle = canvasBackgroundColorConstant;
+	canvasMatrixRainContext.fillRect(0, 0, window.innerWidth, window.innerHeight);
+	canvasMatrixRainContext.fillStyle = canvasTextColorConstant;
 }
